2fe50e264b3c32f42ff16accbdfee5a7ec5f3e70
mrDennisA <Dennis.Alekseev@gmail.com>
2024-10-31 10:43:39 +0100

    api url

--- src/constants/api.jsx
+++ src/constants/api.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = process.env.API_URL;
 
 export const HOME_URL = BASE_URL + "/home";
 export const PROJECTS_URL = BASE_URL + "/projects";
