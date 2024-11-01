8921d84825c5af64a7dae1f42d98a8fcd71d8210
mrDennisA <Dennis.Alekseev@gmail.com>
2024-11-01 19:46:13 +0100

    update frontpage

--- src/constants/api.jsx
+++ src/constants/api.jsx
@@ -1,5 +1,4 @@
 export const BASE_URL = process.env.API_URL;
-export const API_URL = BASE_URL + "/api";
 
-export const HOME_URL = API_URL + "/home";
-export const PROJECTS_URL = API_URL + "/projects";
+export const HOME_URL = BASE_URL + "/api/home";
+export const PROJECTS_URL = BASE_URL + "/api/projects";
