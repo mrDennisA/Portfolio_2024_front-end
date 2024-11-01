eff429cea309c04e21a1008da3f37776bb250fbf
mrDennisA <Dennis.Alekseev@gmail.com>
2024-11-01 20:45:46 +0100

    img

--- src/components/Banner/page.jsx
+++ src/components/Banner/page.jsx
@@ -17,13 +17,13 @@ export default function Banner({ data }) {
   return (
     <section className={styles.container}>
       <Parallax type="background" styles={styles.parallax}>
-        {/* <img src={BASE_URL + imgBanner[0].url} alt={imgBanner[0].alt} style={{ width: "100%", height: "100%", objectFit: "cover" }} /> */}
-        <DynamicImage url={BASE_URL + imgBanner[0].url} alt={imgBanner[0].alt} />
+        <img src={BASE_URL + imgBanner[0].url} alt={imgBanner[0].alt} style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+        {/* <DynamicImage url={BASE_URL + imgBanner[0].url} alt={imgBanner[0].alt} /> */}
       </Parallax>
       <Parallax type="text" styles={styles.parallax}>
         <Title client={client} title={title} color={color} />
       </Parallax>
-      {imgBanner[1] && <DynamicImage styles={styles.img} url={BASE_URL + imgBanner[1].url} alt={imgBanner[1].alt} />}
+      {/* {imgBanner[1] && <DynamicImage styles={styles.img} url={BASE_URL + imgBanner[1].url} alt={imgBanner[1].alt} />} */}
     </section>
   );
 }
