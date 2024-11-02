8984d0afb4b48694099fbeb429794404d65eed9c
mrDennisA <Dennis.Alekseev@gmail.com>
2024-11-02 14:32:26 +0100

    test speed

--- src/components/ImageLoader/page.jsx
+++ src/components/ImageLoader/page.jsx
@@ -8,12 +8,13 @@ export default async function ImageLoader({ src, alt = "", styles = "" }) {
   return (
     <Image
       className={styles}
-      src={img.src}
+      src={src}
       alt={alt}
-      placeholder="blur"
-      blurDataURL={base64}
-      width={img.width}
-      height={img.height}
+      // placeholder="blur"
+      // blurDataURL={base64}
+      // width={img.width}
+      // height={img.height}
+      fill
       quality={100}
       sizes="(max-width: 1280px)100vw"
       style={{ width: "100%", height: "100%", objectFit: "cover" }}
