7494ddfde4c092c8209e3d2870d6f30122e73735
mrDennisA <Dennis.Alekseev@gmail.com>
2024-11-06 22:05:26 +0100

    card list

--- src/components/Project/Card/page.jsx
+++ src/components/Project/Card/page.jsx
@@ -21,12 +21,12 @@ export default async function Card({ data }) {
         <div className={styles.imgContainer}>
           <ImageLoader src={BASE_URL + data.imgCard.url} alt={data.imgCard.alt} />
         </div>
-        <TextInView styles={styles.description} color={data.colorBG}>
-          {/* <div className={styles.description} style={{ backgroundColor: data.colorBG }}> */}
-          <span className={styles.client}>{data.client}</span>
-          <h3 className={styles.title}>{data.title}</h3>
-          {/* </div> */}
-        </TextInView>
+        {/* <TextInView styles={styles.description} color={data.colorBG}> */}
+        {/* <div className={styles.description} style={{ backgroundColor: data.colorBG }}> */}
+        <span className={styles.client}>{data.client}</span>
+        <h3 className={styles.title}>{data.title}</h3>
+        {/* </div> */}
+        {/* </TextInView> */}
       </TransitionLink>
     </div>
   );
