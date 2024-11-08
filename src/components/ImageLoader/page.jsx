38e37d259bf3bf20ac8b0abf5e633377ed3f8f09
mrDennisA <Dennis.Alekseev@gmail.com>
2024-11-08 21:23:55 +0100

    clean up components

--- src/components/ImageLoader/page.jsx
+++ src/components/ImageLoader/page.jsx
@@ -1,4 +1,5 @@
-import ImageDynamic from "../ImageDynamic/page";
+import Image from "next/image";
+
 import getImage from "@/util/getImage";
 
 // Render
@@ -6,5 +7,16 @@ export default async function ImageLoader({ data, styles, loading }) {
   const { url, alt } = data;
   const { base64, color, width, height } = await getImage(url);
 
-  return <ImageDynamic data={{ url, alt, width, height, base64, color }} styles={styles} loading={loading} />;
+  return (
+    <Image
+      className={styles}
+      src={url}
+      alt={alt}
+      width={width}
+      height={height}
+      loading={loading}
+      sizes="(max-width: 1280px)100vw"
+      style={{ width: "100%", height: "100%", objectFit: "cover" }}
+    />
+  );
 }
--- src/components/Project/Detail/page.jsx
+++ src/components/Project/Detail/page.jsx
@@ -1,12 +1,30 @@
 //Components
-import ImgType from "@/components/ImageType/page";
+import Introduction from "@/components/Introduction/page";
+import ScrollinView from "@/components/FramerMotion/ScrollInView/page";
+import ImageLoader from "@/components/ImageLoader/page";
 
 //Styles
-import Introduction from "@/components/Introduction/page";
+import styles from "./detail.module.css";
 
 // Render
 export default function Detail({ data }) {
   // console.log(data);
 
-  return <>{data.map((item, index) => (item.name === "text" ? <Introduction key={index} data={item} /> : <ImgType key={index} data={item} />))}</>;
+  return (
+    <>
+      {data.map((item, index) =>
+        item.name === "text" ? (
+          <Introduction key={index} data={item} />
+        ) : (
+          <section key={index} className={styles[item.type]}>
+            {item.img.map((img, index2) => (
+              <ScrollinView key={index2} styles={styles.parallax}>
+                <ImageLoader data={img} />
+              </ScrollinView>
+            ))}
+          </section>
+        )
+      )}
+    </>
+  );
 }
